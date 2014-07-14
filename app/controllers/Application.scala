ee0548f6d11094f6bc2afe4a9c677a040fff65d3
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-07-15 00:51:55 +0100

    1. defining a trait with generic repositories. 2. Using properly the actions in the controllers to return Future

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,54 +1,83 @@
 package controllers
 
-import models.{FakeContentRepository, FakeTVChannelRepositoy}
+import models._
+import play.api.libs.json._
 import play.api.mvc._
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 
-//object Application {
-//
-//  def apply() = new Application()
-//}
-
-//class Application(tvRepository: TVRepository) extends Controller {
+object Application extends Application {
+  override val channelRepository = TVChannelRepository("tvChannel")
+  override val contentRepository = TVContentRepository("tvContent")
+}
 
 
+trait Application extends Controller {
 
-object Application extends Controller {
+  implicit object TVProgramFormat extends Format[TVProgram] {
 
-  val tvChannelRepository = FakeTVChannelRepositoy
-  
-  val tvContentRepository = new FakeContentRepository()
+
+    def writes(tvProgram: TVProgram): JsValue = {
+      val tvProgramSeq = Seq(
+        "channelName" -> JsString(tvProgram.channelName),
+        "programName" -> JsString(tvProgram.programName),
+        "start" -> JsNumber(tvProgram.start),
+        "end" -> JsNumber(tvProgram.end),
+        "typeProgram" -> JsString(tvProgram.typeProgram)
+      )
+      JsObject(tvProgramSeq)
+    }
+
+    def reads(json: JsValue): JsResult[TVProgram] = {
+      JsSuccess(TVProgram("", "", 0L, 0L, ""))
+    }
+  }
+
+  implicit object TVChannelFormat extends Format[TVChannel] {
+
+    def writes(tvChannel: TVChannel): JsValue = {
+      val tvChannelSeq = Seq(
+        "name" -> JsString(tvChannel.name),
+        "language" -> JsString(tvChannel.language)
+      )
+      JsObject(tvChannelSeq)
+    }
+
+    def reads(json: JsValue): JsResult[TVChannel] = {
+      JsSuccess(TVChannel("", ""))
+    }
+  }
+
+  val channelRepository : ChannelRepository = ???
+  val contentRepository : ContentRepository = ???
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   def channels = Action.async {
-    Future.successful {
-      Ok("")//(Json.toJson(tvChannelRepository.listOfTVChannels()))
+    channelRepository.listOfTVChannels().map {
+      channels => Ok(Json.toJson(channels))
     }
   }
 
   def currentContent(channelName: String) = Action.async {
-    Future.successful {
-//      Ok(Json.toJson(tvContentRepository.findCurrentContentByChannel(channelName)))
-      Ok("")
+    contentRepository.findCurrentContentByChannel(channelName).map {
+      tvProgram => Ok(Json.toJson(tvProgram))
     }
   }
 
   def contentLeft(channelName: String) = Action.async {
-    Future.successful {
-//      Ok(Json.toJson(tvContentRepository.findLeftContentByChannel(channelName)))
-      Ok("")
+    contentRepository.findLeftContentByChannel(channelName).map {
+      tvPrograms => Ok(Json.toJson(tvPrograms))
     }
   }
 
   def allContent(channelName: String) = Action.async {
-    Future.successful {
-      Ok("")
-//      Ok(Json.toJson(tvContentRepository.findDayContentByChannel(channelName)))
+    contentRepository.findDayContentByChannel(channelName).map {
+      tvPrograms => Ok(Json.toJson(tvPrograms))
     }
   }
+
 }
