51e48796a01447da3c000b015a1ebcb9a1706f89
alvarovilaplana <alvaro.vilaplana@gmail.com>
2014-12-19 12:22:37 +0000

    adding the github ribbons

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,31 @@
+// @SOURCE:/Users/alvaro/personal/tv-api/conf/routes
+// @HASH:3bba2f60a224f95950c04f772956c2c7141e5a53
+// @DATE:Sun Oct 19 21:44:06 BST 2014
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseTVRootController TVRootController = new controllers.ReverseTVRootController();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseTVChannelProviderController TVChannelProviderController = new controllers.ReverseTVChannelProviderController();
+public static final controllers.ReverseTVContentController TVContentController = new controllers.ReverseTVContentController();
+public static final controllers.ReverseTVChannelController TVChannelController = new controllers.ReverseTVChannelController();
+public static final controllers.ReverseTVChannelCategoryController TVChannelCategoryController = new controllers.ReverseTVChannelCategoryController();
+public static class javascript {
+public static final controllers.javascript.ReverseTVRootController TVRootController = new controllers.javascript.ReverseTVRootController();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseTVChannelProviderController TVChannelProviderController = new controllers.javascript.ReverseTVChannelProviderController();
+public static final controllers.javascript.ReverseTVContentController TVContentController = new controllers.javascript.ReverseTVContentController();
+public static final controllers.javascript.ReverseTVChannelController TVChannelController = new controllers.javascript.ReverseTVChannelController();
+public static final controllers.javascript.ReverseTVChannelCategoryController TVChannelCategoryController = new controllers.javascript.ReverseTVChannelCategoryController();
+}
+public static class ref {
+public static final controllers.ref.ReverseTVRootController TVRootController = new controllers.ref.ReverseTVRootController();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseTVChannelProviderController TVChannelProviderController = new controllers.ref.ReverseTVChannelProviderController();
+public static final controllers.ref.ReverseTVContentController TVContentController = new controllers.ref.ReverseTVContentController();
+public static final controllers.ref.ReverseTVChannelController TVChannelController = new controllers.ref.ReverseTVChannelController();
+public static final controllers.ref.ReverseTVChannelCategoryController TVChannelCategoryController = new controllers.ref.ReverseTVChannelCategoryController();
+}
+}
+          
