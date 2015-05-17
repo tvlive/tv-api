3f1e1cbd3b2dbe5ec8a4dd542f080346d8f728b1
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2015-05-17 21:27:47 +0100

    98 - Docker using env variables to set host

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-/root/surferstv-999-SNAPSHOT/bin/surferstv -J-Xmx128m -J-Xms64m -J-XX:MaxPermSize=64m -DProd.mongodbURI=$MONGO_PORT_27017_TCP_ADDR -Drun.mode=Prod -Dhttp.port=80
+/root/surferstv-999-SNAPSHOT/bin/surferstv -J-Xmx128m -J-Xms64m -J-XX:MaxPermSize=64m -DProd.host=$HOST_TVLIVE -DProd.mongodbURI=$MONGO_PORT_27017_TCP_ADDR -Drun.mode=Prod -Dhttp.port=80
